bd4eeb3b786c4ecf73be0c1bc5b52339e58010aa
Richard Green <rich@rdgreen.dev>
2026-08-16 14:26:29 +0100

    Mars preset

--- src/pages/planetoid-presets/index.ts
+++ src/pages/planetoid-presets/index.ts
@@ -1,4 +1,5 @@
 import { iceworldPreset } from './iceworldPreset'
+import { marsPreset } from './marsPreset'
 import { metallicCratersPreset } from './metallicCratersPreset'
 import { potatoPreset } from './potatoPreset'
 import { rockyClassicPreset } from './rockyClassicPreset'
@@ -9,6 +10,7 @@ export type { PlanetoidPreset } from './types'
 export const BUILTIN_PRESETS = [
   rockyClassicPreset,
   iceworldPreset,
+  marsPreset,
   volcanicRiftPreset,
   metallicCratersPreset,
   potatoPreset,
